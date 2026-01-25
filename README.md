05b061b65ffc825ae70c35426c321099ad90d91b
Gurkan <hello@gurkz.me>
2026-01-25 15:41:19 +0100

    setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# superdupercontent
+
+a mod for my private minecraft server (neoforge 1.21.1)
+
+> [!WARNING]
+> do not expect any support
---
+++ src/main/java/me/gurkz/superdupercontent/SuperDuperContent.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2026 Gurkan
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package me.gurkz.superdupercontent;
+
+import com.tterrag.registrate.Registrate;
+import com.tterrag.registrate.util.nullness.NonNullSupplier;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+
+@Mod(SuperDuperContent.MOD_ID)
+public class SuperDuperContent {
+    public static final String MOD_ID = "superdupercontent";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final NonNullSupplier<Registrate> REGISTRATE = NonNullSupplier.lazy(() -> Registrate.create(MOD_ID));
+
+    private static final StackWalker STACK_WALKER = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+
+    public static Registrate registrate() {
+        if (!STACK_WALKER.getCallerClass().getPackageName().startsWith("me.gurkz.superdupercontent"))
+            throw new UnsupportedOperationException("please don't use superdupercontent's registrate instance.");
+        return REGISTRATE.get();
+    }
+
+    public SuperDuperContent(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("SuperDuperContent has started");
+    }
+
+    public static ResourceLocation asResource(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+    }
+}
