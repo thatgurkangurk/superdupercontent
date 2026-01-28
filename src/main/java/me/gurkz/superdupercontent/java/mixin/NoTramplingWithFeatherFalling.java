7f3c2ca46b5207892765a93d14e1104c8bb2282c
Gurkan <hello@gurkz.me>
2026-01-28 19:52:40 +0100

    move feather falling trampling logic to kotlin

--- src/main/java/me/gurkz/superdupercontent/java/mixin/NoTramplingWithFeatherFalling.java
+++ src/main/java/me/gurkz/superdupercontent/java/mixin/NoTramplingWithFeatherFalling.java
@@ -8,15 +8,9 @@
 
 package me.gurkz.superdupercontent.java.mixin;
 
+import me.gurkz.superdupercontent.mixin.FeatherFallingTramplingKt;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -35,15 +29,6 @@ public abstract class NoTramplingWithFeatherFalling {
     private void disableTramplingWithFeatherFalling(
             Level level, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci
     ) {
-        if (!(entity instanceof LivingEntity living)) return;
-
-        Holder.Reference<Enchantment> featherFalling = level
-                .registryAccess()
-                .lookupOrThrow(Registries.ENCHANTMENT)
-                .getOrThrow(Enchantments.FEATHER_FALLING);
-
-        int featherFallingLevel = EnchantmentHelper.getEnchantmentLevel(featherFalling, living);
-
-        if (featherFallingLevel > 0 || living.hasEffect(MobEffects.SLOW_FALLING)) ci.cancel();
+        FeatherFallingTramplingKt.disableTramplingWithFeatherFalling(level, entity, ci);
     }
 }
